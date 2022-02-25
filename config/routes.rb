166fb5481208e75833f83f02b8c6e43be09e7ae9
EC2 Default User <ec2-user@ip-172-31-44-71.ec2.internal>
2022-02-25 05:52:32 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :admin, controllers: {
+    sessions: 'admin/sessions',
+    passwords: 'admin/passwords',
+    registrations: 'admin/registrations'
+  }
+
+  namespace :admin do
+    root 'homes#top'
+    resources :users
+    resources :genres, only: [:index, :create, :edit, :update]
+  end
 end
