fa48192d343725337b96ce0aa4ae68af35ae33a9
EC2 Default User <ec2-user@ip-172-31-44-71.ec2.internal>
2022-02-25 06:41:50 +0000

    [Add]ジャンル管理画面

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,2 +1,15 @@
 class Admin::GenresController < ApplicationController
+  def index
+    @genre = Genre.new
+    @genres = Genre.all
+  end
+  
+  def create
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root 'homes#top'
-    resources :users
     resources :genres, only: [:index, :create, :edit, :update]
   end
 end
