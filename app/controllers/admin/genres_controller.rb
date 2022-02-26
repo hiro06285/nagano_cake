d34d3344dc9805ac10112666385e1033a26416e2
EC2 Default User <ec2-user@ip-172-31-44-71.ec2.internal>
2022-02-26 23:47:04 +0000

    [Fix]ジャンル管理画面

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -3,13 +3,22 @@ class Admin::GenresController < ApplicationController
     @genre = Genre.new
     @genres = Genre.all
   end
-  
+
   def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+    redirect_to admin_genres_path(@genre)
   end
-  
+
   def edit
   end
-  
+
   def update
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
   end
 end
