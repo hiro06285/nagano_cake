a223fcc08e198740353efa4340275909fc0fdaa9
EC2 Default User <ec2-user@ip-172-31-44-71.ec2.internal>
2022-03-02 06:59:35 +0000

    [Add]商品詳細画面

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :genre
   attachment :image
+  def add_tax_non_taxed_price
+    (self.non_taxed_price * 1.10).round
+  end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '313dead6d0f93708f0e298b298d3105f51f5cc551f7e7cfe07ff67cc17b840057821bb469b24739ed3ac8c1d0350ae92241d4a6a515aed86a608d8ebfd3b0e3b'
