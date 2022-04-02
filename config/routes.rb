556ff21e94483dddd23defae54497d08f6d5e9cc
hiro06285 <hiro.06285@gmail.com>
2022-04-02 09:17:08 +0000

    [Add]管理者トップページ

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,7 @@ Rails.application.routes.draw do
     resources :customers, only: [:index, :show, :edit, :update]
     resources :genres, only: [:index, :create, :edit, :update]
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
   end
 
 end
