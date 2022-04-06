b5dedc9b32861d9b22d841d5e931f3f5649291da
hiro06285 <hiro.06285@gmail.com>
2022-04-06 06:30:17 +0000

    [Fix]新着商品

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,7 @@
 class Public::HomesController < ApplicationController
   def top
-    @item = Item.page(params[:page]).per(4)
+    #@item = Item.page(params[:page]).per(4)
+    @item = Item.order('id DESC').limit(4)
   end
 
   def about
